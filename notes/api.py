8f2c9f586705f727f22ee9e3a72ac218174ebc42
Reyaad Rafeek <reyaad510@gmail.com>
2020-01-15 15:04:33 -0500

    Adding in /apinotes form now creates new note

--- notes/api.py
+++ notes/api.py
@@ -3,10 +3,19 @@ from .models import PersonalNote
 
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+
     # Meta tells what part of model we want access
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
+
+    def create(self, validated_data):
+        # import pdb
+        # pdb.set_trace()  # start debugger here
+        # pass
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
 
 
 # Used to visualize
