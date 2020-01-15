48d574d09f0da54ae426907b7d557b0d15c39b09
Reyaad Rafeek <reyaad510@gmail.com>
2020-01-15 14:35:50 -0500

    Installed rest_framework

--- djang_practic/settings.py
+++ djang_practic/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes'
+    'notes',
+    'rest_framework'
 ]
+
+# Will allow read/write permissions for logged in users
+# Read only for anonymous users
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
